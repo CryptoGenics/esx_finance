2b4122ace157d5b9aa8dd495fbf331e6f79efdb9
CryptoGenics <45253987+CryptoGenics@users.noreply.github.com>
2019-10-26 10:28:57 -0500

    Config.Negitive

--- config.lua
+++ config.lua
@@ -1,6 +1,8 @@
 
 Config = {}
 
-Config.CarLoan      	= true	--true if using society_cardealer billing loans
+Config.Negitive         = false  --allow player banks to go negitive to pay bills?
+
+Config.CarLoan          = true	--true if using society_cardealer billing loans
 Config.CarLoantime      = 15 -- hour chosen loan will run payments
 Config.carPaymentperday = 10 --% of loan paid per day
